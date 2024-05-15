027c69187d69efb711a229f1e2392408771fa095
Ashish Sheladiya <ashishsheladiya1997@gmail.com>
2024-05-15 16:47:58 +0530

    Add Calculator Logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-	
+	def self.add(numbers)
+		return "invalid input" if numbers.match?(/,(\n|$)/)
+
+		negatives = numbers.scan(/-\d+/)
+		return "negative numbers not allowed: #{negatives.join(', ')}" if negatives.present?
+		numbers.scan(/[-]?\d+/).map(&:to_i).select { |num| num <= 1000 }.sum
+	end
 end
